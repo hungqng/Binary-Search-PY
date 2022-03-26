ecab3492ba9e3a1b1317f9f0c709c905eadcb6ea
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-03-26 11:23:10 -0700

    Add files via upload

--- Binary Search.py
+++ Binary Search.py
@@ -15,4 +15,14 @@ class Solution:
         # index = bisect.bisect_left(nums, target)
         # return index if index < len(nums) and nums[index] == target else -1
 
-        # Solution 3
+        # Solution 3 O(log n)
+        # l, r = 0, len(nums) - 1
+        # while l <= r:
+        #     mid = (l + r) // 2
+        #     if nums[mid] < target:
+        #         l = mid + 1
+        #     elif nums[mid] > target:
+        #         r = mid - 1
+        #     else:
+        #         return mid
+        # return -1
